720ccebe3952b2884298027cf1a50b5f51351cc9
Karim Shehadeh <kshehadeh@millennialmedia.com>
2012-11-03 20:08:19 -0400

    replaced my email address with dan's

--- contact.php
+++ contact.php
@@ -1,8 +1,8 @@
 <?php
 
 
-//$EmailFrom = "dan@dantrachtman.com";
-$EmailFrom = "karim.shehadeh@gmail.com";
+$EmailFrom = "dan@dantrachtman.com";
+//$EmailFrom = "karim.shehadeh@gmail.com";
 
 $Name = trim(stripslashes($_POST['name']));
 
